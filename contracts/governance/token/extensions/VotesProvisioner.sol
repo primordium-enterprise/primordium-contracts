2e5ebc28d3b0e71bb4f2aa02b34230d0204bea97
BCJdevelopment <bcj@bcjdevelopment.com>
2023-07-08 01:58:32 -0600

    setting up deposit checks

--- contracts/governance/token/extensions/VotesProvisioner.sol
+++ contracts/governance/token/extensions/VotesProvisioner.sol
@@ -18,6 +18,14 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
  */
 abstract contract VotesProvisioner is Votes, ExecutorControlled {
 
+    enum ProvisionModes {
+        Founding, // Initial mode for tokens, allows deposits/withdrawals at all times
+        Governance, // No deposits allowed during Governance mode
+        Funding // deposits/withdrawals are fully allowed during Funding mode
+    }
+
+    ProvisionModes private _provisionMode;
+
     IERC20 private immutable _baseAsset; // The address for the DAO's base ERC20 asset.
 
     uint256 private _tokenPrice; // The price of each base token, in the baseAsset units
@@ -31,12 +39,23 @@ abstract contract VotesProvisioner is Votes, ExecutorControlled {
         _tokenPrice = initialTokenPrice;
     }
 
-    function baseAsset() public view returns(IERC20) {
-        return _baseAsset;
+    function baseAsset() public view returns(address) {
+        return address(_baseAsset);
     }
 
-    function deposit() public payable {
+    function _preDeposit(uint256 amount) internal virtual {
+        require(_provisionMode != ProvisionModes.Governance, "VotesProvisioner: Deposits are not available.");
+        require(amount > 0, "VotesProvisioner: Amount of base asset must be greater than zero.");
+    }
 
+    function deposit() public payable virtual {
+        require(address(_baseAsset) == address(0), "VotesProvisioner: Base asset is not set to ETH.");
+        _preDeposit(msg.value);
+    }
+
+    function deposit(uint256 amount) public virtual {
+        require(address(_baseAsset) != address(0), "VotesProvisioner: Base asset is set to ETH.");
+        _preDeposit(amount);
     }
 
     function withraw() public payable {
