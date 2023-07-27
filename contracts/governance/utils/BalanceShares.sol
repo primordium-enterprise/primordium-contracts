288f31d8085466da9f34617d5dea7b741e9c6fbe
BCJdevelopment <bcj@bcjdevelopment.com>
2023-07-26 20:37:43 -0600

    packing in AccountShare variables

--- contracts/governance/utils/BalanceShares.sol
+++ contracts/governance/utils/BalanceShares.sol
@@ -3,6 +3,8 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
 uint constant MAX_BPS = 10_000;
 
 library BalanceShares {
@@ -11,22 +13,24 @@ library BalanceShares {
         uint256 _totalBps; // Tracks the current total basis points for all accounts currently receiving balance shares
         uint256[] _balances;
         mapping(address => AccountShare) _accounts;
+        mapping(address => mapping(address => bool)) _accountApprovals;
     }
 
     struct AccountShare {
-        uint bps; // The basis points share of this account
-        uint removableAt; // A timestamp (in UTC seconds) at which the revenue share can be removed by the DAO
-        uint startIndex;
-        uint endIndex;
-        uint lastPulledIndex;
-        uint lastPulledBalance;
+        uint64 bps; // The basis points share of this account
+        uint64 createdAt; // A timestamp indicating when this account share was created
+        uint64 removableAt; // A timestamp (in UTC seconds) at which the revenue share can be removed by the DAO
+        uint64 lastWithdrawnAt; // A timestamp (in UTC seconds) at which the revenue share was last withdrawn
+        uint256 startIndex;
+        uint256 endIndex;
+        uint256 lastBalanceIndex;
+        uint256 lastBalance;
     }
 
     struct NewAccountShare {
         address account;
         uint bps;
         uint removableAt;
-        uint endIndex;
     }
 
     function addAccountShares(
@@ -45,16 +49,19 @@ library BalanceShares {
 
         // Loop through accounts and track changes
         uint totalBps = self._totalBps;
+        uint64 currentTimestamp = uint64(block.timestamp); // Cache timestamp in memory to save gas
         for (uint i = 0; i < newAccountShares.length;) {
             NewAccountShare memory nas = newAccountShares[i];
             totalBps += nas.bps;
             self._accounts[nas.account] = AccountShare({
-                bps: nas.bps,
-                removableAt: nas.removableAt,
+                bps: SafeCast.toUint64(nas.bps),
+                createdAt: currentTimestamp,
+                removableAt: SafeCast.toUint64(nas.removableAt),
+                lastWithdrawnAt: currentTimestamp,
                 startIndex: currentBalancesIndex,
-                endIndex: nas.endIndex,
-                lastPulledIndex: currentBalancesIndex,
-                lastPulledBalance: 0
+                endIndex: 0,
+                lastBalanceIndex: currentBalancesIndex,
+                lastBalance: 0
             });
             unchecked {
                 i++;
