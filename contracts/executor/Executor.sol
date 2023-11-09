7d0ed244bf49434a2c7556e8e5a173b612413afc
BCJdevelopment <bcj@bcjdevelopment.com>
2023-11-08 23:36:27 -0700

    started setting up Executor initialization

--- contracts/executor/Executor.sol
+++ contracts/executor/Executor.sol
@@ -4,13 +4,19 @@
 pragma solidity ^0.8.20;
 
 import "./base/Treasurer.sol";
+import "contracts/utils/AuthorizeInitializer.sol";
 
 /**
  * @title Executor - The deployable executor contract.
  *
  * @author Ben Jett - @BCJdevelopment
  */
-abstract contract Executor is Treasurer {
+abstract contract Executor is Initializable, AuthorizeInitializer, Treasurer {
 
+    function initializeExecutor(
+        bytes calldata params
+    ) external initializer authorizeInitializer {
+
+    }
 
 }
