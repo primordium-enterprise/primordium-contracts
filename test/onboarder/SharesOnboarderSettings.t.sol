617cf16fa0a1fa0aedee44b552c95225c7a8e3ca
BCJdevelopment <bcj@bcjdevelopment.com>
2024-01-03 19:47:27 -0700

    testing setTreasury()

--- test/onboarder/SharesOnboarderSettings.t.sol
+++ test/onboarder/SharesOnboarderSettings.t.sol
@@ -5,10 +5,46 @@ import {BaseTest} from "test/Base.t.sol";
 import {ISharesOnboarder} from "src/onboarder/interfaces/ISharesOnboarder.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {OwnableUpgradeable} from "src/utils/OwnableUpgradeable.sol";
+import {ERC165Verifier} from "src/libraries/ERC165Verifier.sol";
+import {ITreasury} from "src/executor/interfaces/ITreasury.sol";
+import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
+contract ERC165Contract is ERC165 { }
 
 contract SharesOnboarderSettingsTest is BaseTest {
     function setUp() public virtual override {
         super.setUp();
+    }
+
+    function test_SetTreasury() public {
+        // Only owner can update treasury
+        vm.prank(users.maliciousUser);
+        vm.expectRevert(abi.encodeWithSelector(OwnableUpgradeable.OwnableUnauthorizedAccount.selector, users.maliciousUser));
+        onboarder.setTreasury(address(0x01));
+
+        // Cannot set to self
+        vm.prank(onboarder.owner());
+        vm.expectRevert(abi.encodeWithSelector(ISharesOnboarder.InvalidTreasuryAddress.selector, address(onboarder)));
+        onboarder.setTreasury(address(onboarder));
+
+        // Cannot set to address(0)
+        vm.prank(onboarder.owner());
+        vm.expectRevert(abi.encodeWithSelector(ISharesOnboarder.InvalidTreasuryAddress.selector, address(0)));
+        onboarder.setTreasury(address(0));
+
+        // Invalid interface support
+        address erc165Contract = address(new ERC165Contract());
+        vm.prank(onboarder.owner());
+        vm.expectRevert(abi.encodeWithSelector(ERC165Verifier.InvalidERC165InterfaceSupport.selector, erc165Contract, type(ITreasury).interfaceId));
+        onboarder.setTreasury(erc165Contract);
+
+        // Valid treasury
+        address currentTreasury = address(onboarder.treasury());
+        address newTreasury = address(executor);
+        vm.prank(onboarder.owner());
+        vm.expectEmit(false, false, false, true, address(onboarder));
+        emit ISharesOnboarder.TreasuryChange(currentTreasury, newTreasury);
+        onboarder.setTreasury(newTreasury);
     }
 
     function test_SetQuoteAsset() public {
